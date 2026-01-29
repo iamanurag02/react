554a29b278a075b58ac59b2e9793f49062a0e009
Anurag Pathak <anurag.pathak0207@gmail.com>
2026-01-29 04:48:54 +0000

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='dad'>
+        <h1 id="child"> Hello</h1>
+        <h1 id="child2"> hello papa</h1>
+      </div>
+    <div id='chacha'></div>
+    </>
+  )
+}
+
+export default App
